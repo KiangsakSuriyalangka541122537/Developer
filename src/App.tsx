5328a9ca5d27c02e2b1cf99201f589f8271c1428
TopFyNaJa <kiangsak.top@gmail.com>
2026-03-24 11:08:18 +0700

    refactor: Remove unused DocumentGenerator route
    The DocumentGenerator page and its associated route were not being utilized and have been removed to streamline the application's navigation and code.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import RequestList from './pages/RequestList';
 import WorkloadOverview from './pages/WorkloadOverview';
 import Profile from './pages/Profile';
 import Users from './pages/Users';
-import DocumentGenerator from './pages/DocumentGenerator';
 import { useAppStore } from './store';
 
 function ProtectedRoute({ children, allowedRoles }: { children: React.ReactNode, allowedRoles?: string[] }) {
@@ -59,11 +58,6 @@ export default function App() {
           <Route path="users" element={
             <ProtectedRoute allowedRoles={['approver']}>
               <Users />
-            </ProtectedRoute>
-          } />
-          <Route path="generator" element={
-            <ProtectedRoute>
-              <DocumentGenerator />
             </ProtectedRoute>
           } />
         </Route>
